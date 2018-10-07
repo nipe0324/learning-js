66d629b96dfd57c4f39afa46aca13e26b9da2fbf
nipe0324 <nipe0324@gmail.com>
2018-10-07 18:00:58 +0900

    remove react_toutorial

--- react_tutorial/app/models/comment.rb
+++
@@ -1,3 +0,0 @@
-class Comment < ActiveRecord::Base
-  validates :author, :text, presence: true
-end
--- react_tutorial/db/seeds.rb
+++
@@ -1,4 +0,0 @@
-Comment.create!([
-  { author: "Pete Hunt", text: "This is one comment." },
-  { author: "Jordan Walke", text: "This is *another* comment." }
-])
