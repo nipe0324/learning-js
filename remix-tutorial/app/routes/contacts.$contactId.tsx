f673ac62a0ca81d69ba5b55270cbd2170ba5a5e9
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-12-10 22:08:09 +0900

    [remix-tutorial] validate params

--- remix-tutorial/app/routes/contacts.$contactId.tsx
+++ remix-tutorial/app/routes/contacts.$contactId.tsx
@@ -1,11 +1,20 @@
 import { json } from "@remix-run/node";
 import { Form, useLoaderData }  from "@remix-run/react";
 import { getContact } from "../data";
+import invariant from "tiny-invariant";
+
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import type { FunctionComponent } from "react";
 import type { ContactRecord } from "../data";
 
-export const loader = async ({ params }) => {
+export const loader = async ({
+  params
+}: LoaderFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param")
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
   return json({ contact });
 }
 
