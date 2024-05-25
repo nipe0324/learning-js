a95629ef6a0f9dd4f3b4ca19510910fdf6e0e896
nipe0324 <nipe0324@gmail.com>
2024-05-25 22:45:04 +0900

    [nextjs-netflix-clone] current user and profile page

--- nextjs-netflix-clone/pages/auth.tsx
+++ nextjs-netflix-clone/pages/auth.tsx
@@ -28,10 +28,9 @@ const Auth = () => {
         email,
         password,
         redirect: false,
-        callbackUrl: '/',
       });
 
-      router.push('/');
+      router.push('/profiles');
     } catch (error) {
       console.log(error);
     }
@@ -95,13 +94,13 @@ const Auth = () => {
             <div className='flex flex-row items-center gap-4 mt-8 justify-center'>
               <div
                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer haver:opacity-80 transition"
-                onClick={() => signIn('google', { callbackUrl: '/' })}
+                onClick={() => signIn('google', { callbackUrl: '/profiles' })}
               >
                 <FcGoogle size={30} />
               </div>
               <div
                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer haver:opacity-80 transition"
-                onClick={() => signIn('github', { callbackUrl: '/' })}
+                onClick={() => signIn('github', { callbackUrl: '/profiles' })}
               >
                 <FaGithub size={30} />
               </div>
