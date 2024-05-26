56ca9d404127ea47343b68969331348f287049dc
nipe0324 <nipe0324@gmail.com>
2024-05-26 11:12:08 +0900

    [nextjs-netflix-clone] watch movie

--- nextjs-netflix-clone/components/MovieCard.tsx
+++ nextjs-netflix-clone/components/MovieCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 import { BsFillPlayFill } from "react-icons/bs";
 import FavoriteButton from "./FavoriteButton";
@@ -10,6 +11,8 @@ interface MovieCardProps {
 const MovieCard: React.FC<MovieCardProps> = ({
   data,
 }) => {
+  const router = useRouter();
+
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
       <img
@@ -28,7 +31,7 @@ const MovieCard: React.FC<MovieCardProps> = ({
           <div className="flex flex-row items-center gap-3">
             <div
               className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center hover:bg-neutral-300"
-              onClick={() => {}}
+              onClick={() => router.push(`/watch/${data?.id}`)}
             >
               <BsFillPlayFill size={30} />
             </div>
